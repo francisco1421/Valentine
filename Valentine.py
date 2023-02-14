9b3f996d698e4fc368d540fb4885141f21d0b38a
francisco1421 <125355686+francisco1421@users.noreply.github.com>
2023-02-14 03:42:09 -0600

    Add files via upload

---
+++ Valentine.py
@@ -0,0 +1,64 @@
+print('Hola amor')
+
+print (  'tres espacios hacia abajo' )
+
+print("Este es un programa disenado para funcionar por siempre")
+input ("Presiona una tecla para continuar") 
+
+print("A continuacion, si sabes las respuestas correctas, encontraras de mi los recuedos de mi corazon")
+input ("Presiona una tecla para continuar")
+print("Quiero ser el hombre perfecto para ti, pero te necesito mas cerca de mi")
+print('Escribiendoooooo.......')
+print("Que empiece el juego mi Valentine")
+
+Fecha_de_cumpleanos = input("Ingresa la fecha de tu cumpleanos: (dd/mm/aaaa) ")
+if Fecha_de_cumpleanos == "05/03/1988":
+    print("Eres la musa que me inspira a crear, a crear fantasia, poema e ilusiones, contigo soy un loco sonador que busca a ti para seguir mi vida contigo")
+else:
+    print("Error vuelve a intentarlo luego")
+
+Fecha_hoy = input("Ingresa la fecha de hoy: (dd/mm/aaaa) ")
+if Fecha_hoy == "14/02/2023":
+    print ("Cada vez que imagino viajar a lugares como la India, Rusia y Japón, lo hago con la esperanza de tener a mi lado a la persona que más amo en este mundo: mi novia. Me encantaría explorar juntos los colores, los sabores y los sonidos de lugares tan fascinantes, rodeados de la belleza de su presencia y la calidez de su amor. Quiero que cada momento sea un recuerdo que atesorar para siempre, y por eso no hay nada que me haría más feliz que compartir estas aventuras con ella. Espero que ella también lo desee, porque con ella a mi lado, cualquier lugar en el mundo se convierte en un paraíso. La amo más allá de las palabras y no puedo esperar para crear juntos nuevos recuerdos inolvidables.")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+nombre_jenn = input("Ingresa tus nombres completos: ")
+if nombre_jenn == "Jennyfer Poleth Cardona Garcia" or nombre_jenn == "jennyfer poleth cardona garcia":
+    print ("Quiero que sepas que mi amor por ti es real y que quiero pasar el resto de mi vida demostrándotelo.")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+nombre_gato = input("Ingresa nombre de nuestro gato: ")
+if nombre_gato == "Bernardo" or nombre_gato == "bernardo" :
+    print ("Uno de los recuerdos más memorables que tengo junto a mi novia fue durante nuestro viaje a Punta Cana. Mientras disfrutábamos de la playa, vimos en la distancia una tormenta eléctrica que se acercaba rápidamente. Decidimos quedarnos en la orilla del mar, viendo cómo los rayos iluminaban el cielo y escuchando los truenos que retumbaban en nuestros oídos. A pesar de la lluvia que caía y las hormigas que subían por nuestras piernas, ese momento fue mágico, como si el mundo se detuviera solo para que pudiéramos disfrutar juntos de la belleza de la naturaleza. Esa noche, nos fuimos a dormir con una sonrisa en nuestros rostros, sabiendo que habíamos vivido un momento especial que siempre recordaríamos.")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+nombre_gata = input("Ingresa nombre de nuestra gata: ")
+if nombre_gata == "Antonela" or nombre_gata == "antonela":
+    print ("Uno de los recuerdos más emocionantes que tengo con mi novia fue durante un viaje a Bogotá, donde en una tarde compramos trajes de baño y cepillos de dientes para nuestra escapada a la playa. Con el tiempo en contra, corrimos para encontrar los trajes perfectos y los accesorios necesarios antes de dirigirnos al aeropuerto. Fue una carrera contra el reloj, pero lo logramos juntos. En ese momento, nos dimos cuenta de que podíamos confiar el uno en el otro, incluso en situaciones estresantes. Recuerdo con cariño cómo nos apoyamos mutuamente y nos reímos durante la frenética compra. Finalmente, llegamos al aeropuerto justo a tiempo, pero la emoción de la experiencia no nos dejó durante todo el viaje. Ese momento me enseñó que las aventuras más divertidas pueden ocurrir en los momentos más inesperados, especialmente cuando estás con la persona adecuada.")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+fecha_aniversario = input("Ingresa la fecha de nuestro aniversario: (dd/mm/aaaa)")
+if fecha_aniversario == ("15/07/2015"):
+    print ("Uno de los momentos más mágicos que he compartido con mi novia fue durante una caminata por la playa del río Napo. Mientras paseábamos, el tiempo parecía detenerse y solo podía enfocarme en su hermoso rostro. Miré fijamente sus ojos y sentí como si pudiera ver la profundidad de su alma reflejada en ellos. En ese momento, me di cuenta de que la naturaleza y el amor pueden ser tan profundos como el río que teníamos delante. Juntos, caminamos en armonía y disfrutamos del sonido de las olas y el canto de los pájaros. Fue un momento de conexión profunda que me dejó sin palabras, y siempre atesoraré esa sensación de paz y amor que sentí en ese instante. A veces, los momentos más simples y naturales pueden ser los más hermosos y memorables, y estoy agradecido por haber compartido ese momento con la persona que amo.")
+if fecha_aniversario == "13/11/2013)":
+    print ("Casi pero no")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+numero_espalda = input("Ingresa el numero de mi espalda: ")
+if numero_espalda == ("142"):
+    print ("Uno de los recuerdos más emocionantes que tengo con mi novia fue cuando fuimos a Jamaica y ella experimentó un mundo completamente nuevo. Desde el momento en que llegamos, pudimos sentir la energía y la vibración de la isla. Mi novia estaba fascinada por todo lo que veía: los paisajes tropicales, la música, la comida, la gente, y la cultura en general. Fue un momento especial ver cómo se abría a nuevas experiencias y estaba dispuesta a probar cosas nuevas. Juntos, descubrimos playas vírgenes, nadamos en aguas cristalinas, y bailamos al son del reggae. Fue una aventura inolvidable que nos unió aún más como pareja. Esa experiencia me recordó que siempre hay algo nuevo por descubrir y que la vida es aún más emocionante cuando se experimenta con alguien que amas. Guardaré ese recuerdo con cariño para siempre.")
+else:
+    print ("Error vuelve a intentarlo luego")
+
+print ("Que tengas un buen dia mi Valentine, espero verte hoy")
+Repetir_juego = input("Puedes iniciar el programa y volverlo a intentar")
+
+if nombre_jenn == ("Jennyfer Poleth Cardona Garcia") or "jennyfer poleth cardona garcia" and nombre_gato == ("Bernardo") or "bernardo" and nombre_gata == ("Antonela") or "antonela" and numero_espalda == ("142") and Fecha_de_cumpleanos == ("05/03/1988"):
+    print ("TE AMO MI SIRENA")
+else:
+      print ("No has cumplido todas las repuestas, Fin del juego")
